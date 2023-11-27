fc2271e2e5ee8cc2da90e2bbb6a3af92429443c3
Zapupa <vitaliy2005rus93@gmail.com>
2023-11-27 21:30:09 +0500

    Обновлено модальное окно, добавлен поиск

--- Streamline/src/components/SelectModal/SelectModal.jsx
+++ Streamline/src/components/SelectModal/SelectModal.jsx
@@ -1,7 +1,44 @@
-import { useModalRootContext, Spinner, Group, Cell } from "@vkontakte/vkui";
+import {
+  Group,
+  Cell,
+  PanelHeader,
+  PanelHeaderButton,
+  Search,
+  Footer,
+} from "@vkontakte/vkui";
+import { useState, useEffect, filter, Fragment } from "react";
+
+const cities = [
+  { center: [55.159901, 61.402547], zoom: 12, name: "Челябинск" },
+  { center: [55.75404, 37.614953], zoom: 12, name: "Москва" },
+  {
+    center: [59.936542, 30.313521],
+    zoom: 12,
+    name: "Санкт-Петербург",
+  },
+];
 
 const SelectModal = () => {
-  return <div className="SelectModal">s</div>;
+  const [search, setSearch] = useState("");
+
+  const onChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const citiesFiltered = cities.filter(
+    ({ name }) => name.toLowerCase().indexOf(search.toLowerCase()) > -1
+  );
+  return (
+    <Fragment>
+      <PanelHeader>Выбор города</PanelHeader>
+      <Group>
+        <Search value={search} onChange={onChange} after={null} />
+        {citiesFiltered.length > 0 &&
+          citiesFiltered.map((city) => <Cell>{city.name}</Cell>)}
+        {citiesFiltered.length === 0 && <Footer>Ничего не найдено</Footer>}
+      </Group>
+    </Fragment>
+  );
 };
 
 export default SelectModal;
