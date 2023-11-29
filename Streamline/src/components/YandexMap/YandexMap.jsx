fe2832f7aa8998f8edfb7eb01805d3b577c7180b
Zapupa <vitaliy2005rus93@gmail.com>
2023-11-29 17:27:11 +0500

    мАЛЕНЬКИЕ ДОРАБОТКИ

--- Streamline/src/components/YandexMap/YandexMap.jsx
+++ Streamline/src/components/YandexMap/YandexMap.jsx
@@ -8,8 +8,7 @@ import {
   Polyline,
   Button,
 } from "@pbe/react-yandex-maps";
-import trail from "../../resources/trails/gorodskoi-bor.gpx";
-// import trail from "../../resources/trails/moskva-pushchino-tesna-june-16-mmxviii.gpx";
+
 const API_KEY = "8ad44015-6f2d-4403-92cb-4daa45aff76c";
 
 const defaultMapState = {
@@ -18,24 +17,6 @@ const defaultMapState = {
 };
 
 const YandexMap = ({ mapState, positions }) => {
-  // const [position, setPosition] = useState("");
-  // setPosition(positions);
-  // const [positions, setPositions] = useState("");
-  // let gpxParser = require("gpxparser");
-  // var gpx = new gpxParser();
-
-  // const componentDidMount = async () => {
-  //   const response = await fetch(trail, { metod: "POST" });
-  //   const gpxDemo = await response.text();
-
-  //   gpx.parse(gpxDemo);
-  //   setPosition(gpx.tracks[0].points.map((p) => [p.lat, p.lon]));
-
-  //   console.log(position);
-  // };
-  // const pivo = () => {
-  //   componentDidMount();
-  // };
   return (
     <div>
       <YMaps
